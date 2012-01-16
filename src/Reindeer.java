564e6bb89505385a0b14cc47959459e2e60c6a4f
ian <ithunt0@gmail.com>
2012-01-16 17:13:03 -0500

    Reindeer names

--- src/Reindeer.java
+++ src/Reindeer.java
@@ -3,6 +3,9 @@
  * @date 1/16/12
  */
 public class Reindeer extends Thread {
+    
+    private final static String[] reindeer =
+            {"Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen", "Rudolph"};
 
     public Reindeer(String name) {
         super(name);
