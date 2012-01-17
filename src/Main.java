0b14aff596bbadaf1f9b878d16e12d5c05e8d437
McAfee <McAfee@HANNIBAL-LECTOR.(none)>
2012-01-17 12:37:48 -0800

    Added constants
    Added Reindeer delay

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ public class Main {
 	//list of constants
 	private final int NUM_ELVES = 10;
 	private final int NUM_REINDEER = 9;
-	private final int INIT_DAYS_TO_XMAS = 365;
+	private final int INIT_DAYS_TO_XMAS = 364;
 	private final int ELF_COUNT_WORTH_SANTAS_ATTENTION = 3;
-	private final int SECONDS_PER_DAY = 15;
+	private final int MILLIS_PER_DAY = 15000;
+	private final double INIT_REINDEER_DELAY = INIT_DAYS_TO_XMAS * MILLIS_PER_DAY * .8;
     
     public static void main(String[] args)  {
     	
     	
-
     }
 
 }
