5b15a80fbc1441391e880c4445016710b7a7bdde
McAfee <McAfee@HANNIBAL-LECTOR.(none)>
2012-01-17 12:24:22 -0800

    Added constants
    Now has some.

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,17 @@
  * @date 1/16/12
  */
 public class Main {
+	
+	//list of constants
+	private final int NUM_ELVES = 10;
+	private final int NUM_REINDEER = 9;
+	private final int INIT_DAYS_TO_XMAS = 365;
+	private final int ELF_COUNT_WORTH_SANTAS_ATTENTION = 3;
+	private final int SECONDS_PER_DAY = 15;
     
     public static void main(String[] args)  {
+    	
+    	
 
     }
 
