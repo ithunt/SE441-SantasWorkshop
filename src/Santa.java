52513901204bb2703fc3f3c10afa7f79ebe3d925
McAfee <McAfee@HANNIBAL-LECTOR.(none)>
2012-01-22 21:48:59 -0800

    Santa can solve elves problems

--- src/Santa.java
+++ src/Santa.java
@@ -14,9 +14,28 @@ public class Santa {
     public synchronized void awaken() {
         if(workshop.getWarmingHut().isBroken()) { //&& is christmas
             //presents!
-        } else if(workshop.getElfQueue().size() == 3) {
-            workshop.getElfQueue().clear(); //problems solved
+        } else if(workshop.getElfQueue().size() == 
+        		SantaConstants.ELF_COUNT_WORTH_SANTAS_ATTENTION) {
+        	this.solveElvesProblems(); //problems solved
         }
 
+    }
+    
+    private void solveElvesProblems(){
+    	
+    	synchronized (this){
+    		ElfQueue<Elf> problemElves = workshop.getElfQueue();
+    		Elf problemElf;
+    		
+    		for (int i= 0; 1<SantaConstants.ELF_COUNT_WORTH_SANTAS_ATTENTION; ++i){
+    			try {
+					problemElf = problemElves.take();
+					problemElf.notify(); //problem solved!
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+    		}
+    		
+    	}
     }
 }
