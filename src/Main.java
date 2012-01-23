5e57325ae0067106c44beebfadef534b079dda36
McAfee <McAfee@HANNIBAL-LECTOR.(none)>
2012-01-22 18:16:01 -0800

    Moved constants to distinct class
    Also updated VSD

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,6 @@ import java.util.ArrayList;
  */
 public class Main {
 	
-	//list of constants
-	public static final int NUM_ELVES = 10;
-	public static final int NUM_REINDEER = 9;
-	public static final int INIT_DAYS_TO_XMAS = 364;
-	public static final int ELF_COUNT_WORTH_SANTAS_ATTENTION = 3;
-	public static final int MILLIS_PER_DAY = 15000;
-	public static final double INIT_REINDEER_DELAY = INIT_DAYS_TO_XMAS * MILLIS_PER_DAY * .8;
-	public static final double INIT_ELF_DELAY = INIT_DAYS_TO_XMAS * MILLIS_PER_DAY * .1;
     
     public static void main(String[] args)  {
 
