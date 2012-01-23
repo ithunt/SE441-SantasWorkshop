5377a44d1406d54330fe55c2130b18b4776c6fe4
McAfee <McAfee@HANNIBAL-LECTOR.(none)>
2012-01-22 21:23:00 -0800

    Constants + Queue
    Created Queue, remembered to "add" Constants

---
+++ src/SantaConstants.java
@@ -0,0 +1,14 @@
+
+public class SantaConstants {
+
+	//list of constants
+	public static final int NUM_ELVES = 10;
+	public static final int NUM_REINDEER = 9;
+	public static final int INIT_DAYS_TO_XMAS = 364;
+	public static final int ELF_COUNT_WORTH_SANTAS_ATTENTION = 3;
+	public static final int MILLIS_PER_DAY = 15000;
+	public static final double INIT_REINDEER_DELAY = INIT_DAYS_TO_XMAS * MILLIS_PER_DAY * .8;
+	public static final double INIT_ELF_DELAY = INIT_DAYS_TO_XMAS * MILLIS_PER_DAY * .1;
+	
+	
+}
