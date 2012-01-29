55ead5a58c2c8a4698620c8c215a2e3275b2b564
McAfee <pjm4439@rit.edu>
2012-01-29 14:07:44 -0500

    Removed unneeded constants
    Num_Reindeer and Init_elf_delay

--- src/SantaConstants.java
+++ src/SantaConstants.java
@@ -5,14 +5,12 @@ public class SantaConstants {
 
 	//list of constants
 	public static final int NUM_ELVES = 15;
-	//public static final int NUM_REINDEER = 9;
 	public static final int INIT_DAYS_TO_XMAS = 349;
 	public static final int ELF_COUNT_WORTH_SANTAS_ATTENTION = 3;
 	public static final int MILLIS_PER_DAY = 20;
     public static final int ELF_PROBLEM_MILLIS_LOW = 2000;
     public static final int ELF_PROBLEM_MILLIS_HIGH = 3000;
 	public static final double INIT_REINDEER_DELAY = INIT_DAYS_TO_XMAS * MILLIS_PER_DAY * .8;
-	public static final double INIT_ELF_DELAY = INIT_DAYS_TO_XMAS * MILLIS_PER_DAY * .1;
 	public static final String[] REINDEER_NAMES =
         {"Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen", "Rudolph"};
 	
